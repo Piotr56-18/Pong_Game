ec399fec72b38bf3470827031e7d28893a153bbf
Piotr56-18 <piotr_szymanik@interia.pl>
2024-01-12 20:03:06 +0100

    - Added Ball class - Ball logic added (collision with wall, collision with paddle)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
-
+from ball import Ball
+import time
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
@@ -9,6 +10,7 @@ screen.tracer(0)
 
 paddle_one = Paddle(350, 0)
 paddle_two = Paddle(-350, 0)
+ball = Ball()
 
 screen.listen()
 screen.onkey(fun=paddle_one.move_up, key="Up")
@@ -18,6 +20,19 @@ screen.onkey(fun=paddle_two.move_down, key="s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+    #Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    #Detect collision with paddle
+    if ball.distance(paddle_one) < 50 and ball.xcor() > 320 or ball.distance(paddle_two) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+    #Detect when the ball goes out of bounds
+    if ball.xcor() > 380:
+        ball.reset_round()
+    if ball.xcor() < -380:
+        ball.reset_round()
 
 screen.exitonclick()
